d76c95c2dd1c331492f4823fb95dc28ce0fcb23d
Jonas Wallays <jonas.wallays.u8978@student.hogent.be>
2016-12-19 21:00:56 +0100

    Post method form

---
+++ WindowsOpendeurdagAppClient/GebruikerFormuliers.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using WindowsOpendeurdagAppWeb.Models;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WindowsOpendeurdagAppClient
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class GebruikerFormuliers : Page
+    {
+        HttpClient client;
+
+        public GebruikerFormuliers()
+        {
+            this.InitializeComponent();
+        }
+
+        private void home_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage), null);
+        }
+
+        private async void Button_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            client = new HttpClient();
+            try
+            {
+                 
+                GebruikerFormulier form = new GebruikerFormulier {
+                    Voornaam = this.voornaam.Text,
+                    Achternaam = this.achternaam.Text,
+                    Geboortedatum = DateTime.Parse(this.geboortedatum.ToString()),
+                    Straat = this.adres.Text,
+                    Email = this.email.Text,
+                    Telnr = int.Parse(this.gsmnummer.Text),
+                    Campus = this.campus.SelectedItem.ToString(),
+                    Richting = this.opleiding.SelectedItem.ToString()
+    };
+                var formJson = JsonConvert.SerializeObject(form);
+                var res = await client.PostAsync("http://localhost:64288/api/gebruikerformuliers", new
+                    StringContent(formJson, System.Text.Encoding.UTF8, "application/json"));
+            }
+            catch (Exception ex)
+            {
+               
+            }
+            finally
+            {
+                if (client != null)
+                {
+                    client.Dispose();
+                    client = null;
+                }
+            }
+        }
+    }
+}
---
+++ WindowsOpendeurdagAppClient/Models/GebruikerFormulier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace WindowsOpendeurdagAppWeb.Models
+{
+    public class GebruikerFormulier
+    {
+        public int GebruikerFormulierId { get; set; }
+
+        public string Voornaam { get; set; }
+
+        public string Achternaam { get; set; }
+
+        public DateTime Geboortedatum { get; set; }
+
+        public string Straat { get; set; }
+
+        public string Email { get; set; }
+
+        public int Telnr { get; set; }
+
+        public string Campus { get; set; }
+
+        public string Richting { get; set; }
+    }
+}
