1cec59e9566d330d3e08672651fbde915fcc62ba
DESKTOP-O5ESE9D\sofie <sofie@delaeter.be>
2017-08-28 12:17:19 +0200

    statistieken toegevoegd

--- WindowsOpendeurdagAppClient/Statistieken.xaml.cs
+++ WindowsOpendeurdagAppClient/Statistieken.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -12,6 +15,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using WindowsOpendeurdagAppWeb.Models;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -35,6 +39,80 @@ namespace WindowsOpendeurdagAppClient
         private void adminlogout_Tapped(object sender, TappedRoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(MainPage), null);
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            // system.net import!!
+            HttpClient client = new HttpClient();
+            var jsonstring = await client.GetStringAsync(new Uri("http://localhost:64288/api/gebruikerformuliers"));
+            //json nuget package!!
+            var lst = JsonConvert.DeserializeObject<ObservableCollection<GebruikerFormulier>>(jsonstring);
+            var totaal = lst.Count;
+            var aantalAalst = 0;
+            var aantalBijloke = 0;
+            var aantalLedeganck = 0;
+            var aantalMelle = 0;
+            var aantalMercator = 0;
+            var aantalBedrijfsmanagement = 0;
+            var aantalOfficemanagement = 0;
+            var aantalRetailmanagement = 0;
+            var aantalTIN = 0;
+            foreach (GebruikerFormulier formulier in lst)
+            {
+                try { 
+                if (formulier.Campus.ToLower().Contains("aalst"))
+                {
+                    aantalAalst++;
+                } else if (formulier.Campus.ToLower().Contains("bijloke"))
+                {
+                    aantalBijloke++;
+                }
+                else if (formulier.Campus.ToLower().Contains("ledeganck"))
+                {
+                    aantalLedeganck++;
+                }
+                else if (formulier.Campus.ToLower().Contains("melle"))
+                {
+                    aantalMelle++;
+                }
+                else if (formulier.Campus.ToLower().Contains("mercator"))
+                {
+                    aantalMercator++;
+                }
+
+                if (formulier.Richting.ToLower().Contains("bedrijf"))
+                {
+                    aantalBedrijfsmanagement++;
+                }
+                else if (formulier.Richting.ToLower().Contains("office"))
+                {
+                    aantalOfficemanagement++;
+                }
+                else if (formulier.Richting.ToLower().Contains("retail"))
+                {
+                    aantalRetailmanagement++;
+                }
+                else if (formulier.Richting.ToLower().Contains("informatica"))
+                {
+                    aantalTIN++;
+                }
+                }catch(NullReferenceException ex)
+                {
+                    
+                }
+                Ttotaal.Text = ""+ totaal;
+                TaantalAalst.Text = "" + aantalAalst;
+                TaantalBedrijfsmanagement.Text = "" + aantalBedrijfsmanagement;
+                TaantalBijloke.Text = "" + aantalBijloke;
+                TaantalLedeganck.Text = "" + aantalLedeganck;
+                TaantalMelle.Text = "" + aantalMelle;
+                TaantalMercator.Text = "" + aantalMercator;
+                TaantalRetailmanagement.Text = "" + aantalRetailmanagement;
+                TaantalTIN.Text = "" + aantalTIN;
+                TaantalOfficemanagement.Text = "" + aantalOfficemanagement;
+            }
         }
     }
 }
